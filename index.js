f57e9869c8b0741d6358dc12e261bc0441dd0d5b
GabrielJacquier <gabriel.jacquier@tegra.me>
2016-09-16 22:00:24 -0300

    Start basic flow

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connection Ok!!");
+});
+
+var test = mongoose.Schema({
+    name: String,
+    adress: String
+});
+
+var Test = mongoose.model('Test', test);
+
+var test = new Test({ name: 'Silence', adress: 'um adress' });
+
+test.save(function (err, test) {
+  if (err) return console.error(err);
+  console.log(test.name);
+});
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,5 +40,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
