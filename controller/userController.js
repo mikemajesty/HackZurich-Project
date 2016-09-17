7581d01a0666c2096588300c09377d6e8e1eaec8
mikemajesty <mikemajesty2011@gmail.com>
2016-09-17 01:11:51 -0300

    Save succesfully

--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,7 @@ var userService = require('../service/userService');
 
 
 var save = function (req, res) {
-    var users = req.body.users
+    var users = req.body;
     userService.save(users).then((data) => {
       res.json(data);
     });
