b3903e5c8a6670e7821ec33e3b33c7cb699a16e1
mikemajesty <mikemajesty2011@gmail.com>
2016-09-17 15:29:08 -0300

    Refactor in controller

--- controller/clientController.js
+++ controller/clientController.js
@@ -7,8 +7,10 @@ const clientService = require('../service/clientService');
  * List all clients endpoint.
  */
 const all = (req, res) => {
-    var MS_PER_MINUTE = 60000;
-    var myStartDate = new Date(new Date().getTime() - (5 * MS_PER_MINUTE));
+    var MINUTE = 5;
+    var myStartDate = new Date();
+    myStartDate.setMinutes(myStartDate.getMinutes() - MINUTE);
+    console.log(myStartDate);
     clientService.find({os:{ $in: [null, 'iOS', 'Android']}, seenTime: {$gte: myStartDate.toISOString()} })
     .then((clients) => {
         res.status(200).send(clients);
