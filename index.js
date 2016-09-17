8a9140dbd15f91b2d328de4559030a3297e91f6e
Celso Wo <celso.wo@tegra.me>
2016-09-17 16:13:33 +0200

    log

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(parser());
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/', express.static(__dirname + '/public'));
+// app.use('/', express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
